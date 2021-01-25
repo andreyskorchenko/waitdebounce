133339b607feed218000eb9f3290dc93f24d584d
Andrey Skorchenko <43827745+andreyskorchenko@users.noreply.github.com>
2021-01-25 23:42:20 +0300

    Update debounc.js

--- debounc.js
+++ debounc.js
@@ -1,14 +1,14 @@
 export function debounce(callback, ms) {
-	var timeoutID = undefined;
+  var timeoutID = undefined;
 
-	return function() {
-		var self = this;
-		var args = arguments;
+  return function() {
+    var self = this;
+    var args = arguments;
 
-		clearTimeout(timeoutID);
-		
-		timeoutID = setTimeout(function() {
-			callback.apply(self, args);
-		}, ms);
-	};
+    clearTimeout(timeoutID);
+
+    timeoutID = setTimeout(function() {
+      callback.apply(self, args);
+    }, ms);
+  };
 };
